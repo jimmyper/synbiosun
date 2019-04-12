1a500d232ac18a4508e6a02d076242f87b4d9551
jimmyper <31047554+jimmyper@users.noreply.github.com>
2019-04-12 13:00:49 -0400

    update

--- JavaScript D3/jsts.js
+++
@@ -1,8 +0,0 @@
-var cars = ["BMW", "Volvo", "Saab", "Ford"];
-var i = 0;
-var text = "";
-for (;cars[i];) {
-  text += cars[i] + "<br>";
-  i++;
-}
-document.getElementById("demo").innerHTML = text;
